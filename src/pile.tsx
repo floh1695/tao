13ad8f89dca940e1bcf1605dc87926c833644053
Charles Bayley <cbayley95@gmail.com>
2023-07-18 19:52:50 -0400

    Book #4

--- src/pile.tsx
+++ src/pile.tsx
@@ -137,42 +137,48 @@ export const book4 =
     'Coding',
     'A well-written program is its own Heaven; a poorly-written program is its own Hell.'
   )
-  // .addChapter(
-  //   Chapter.new(1, [
-  //     sentences([
-  //       'A program should be light and agile, its subroutines connected like a string of pearls.',
-  //       'The spirit and intent of the program should be retained throughout.',
-  //       'There should be neither too little nor too much.',
-  //       'Neither needless loops nor useless variables; neither lack of structure nor overwhelming rigidity.',
-  //     ], {}),
-  //     sentences([
-  //       'A program should follow the',
-  //       q('Law of Least Astonishment.'),
-  //       'What is this law?',
-  //       'It is simply that the program should always respond to the users in the way that least astonishes them.',
-  //     ], {}),
-  //     sentences([
-  //       'A program, no matter how complex, should act as a single unit.',
-  //       'The program should be directed by the logic within rather than by outward appearances.',
-  //     ], {}),
-  //     sentences([
-  //       'If the program fails in these requirements, it will be in a state of disorder and confusion.',
-  //       'The only way to correct this is to rewrite the program.',
-  //     ], {}),
-  //   ])
-  // )
-  // .addChapter(
-  //   Chapter.new(2, [
-  //   ])
-  // )
-  // .addChapter(
-  //   Chapter.new(3, [
-  //   ])
-  // )
-  // .addChapter(
-  //   Chapter.new(4, [
-  //   ])
-  // );
+  .addNextChapter(
+    <>
+      <p>A program should be light and agile, its subroutines connected like a string of pearls. The spirit and intent of the program should be retained throughout. There should be neither too little nor too much. Neither needless loops nor useless variables; neither lack of structure nor overwhelming rigidity.</p>
+      <p>A program should follow the <q>Law of Least Astonishment.</q> What is this law? It is simply that the program should always respond to the users in the way that least astonishes them.</p>
+      <p>A program, no matter how complex, should act as a single unit. The program should be directed by the logic within rather than by outward appearances.</p>
+      <p>If the program fails in these requirements, it will be in a state of disorder and confusion. The only way to correct this is to rewrite the program.</p>
+    </>
+  )
+  .addNextChapter(
+    <>
+      <p>A novice asked the Master: <q>I have a program that sometimes runs and sometimes aborts. I have followed the rules of programming, yet I am totally baffled. What is the reason for this?</q></p>
+      <p>
+        The Master replied: <q>
+          You are confused because you do not understand Tao. Only a fool expects rational behavior from his fellow humans. Why do you expect it from a machine that humans have constructed? Computers simulate determinism; only Tao is perfect.
+          <br />
+          The rules of programming are transitory; only Tao is eternal. Therefore, you must contemplate Tao before you receive Enlightenment.
+        </q>
+      </p>
+      <p><q>But how will I know when I have received Enlightenment?</q> asked the novice.</p>
+      <p><q>Your program will run correctly,</q> replied the Master.</p>
+    </>
+  )
+  .addNextChapter(
+    <>
+      <p>The Master was explaining the nature of Tao to one of his novices.</p>
+      <p><q>The Tao is embodied in all software -- regardless of how insignificant,</q> said the Master.</p>
+      <p><q>Is the Tao in a hand-held calculator?</q> asked the novice.</p>
+      <p><q>It is,</q> came the reply.</p>
+      <p><q>Is the Tao in a video game?</q> asked the novice.</p>
+      <p><q>It is even in a video game,</q> said the Master.</p>
+      <p><q>Is the Tao in the <code>DOS</code> for a personal computer?</q> asked the novice.</p>
+      <p>The Master coughed and shifted his position slightly. <q>The lesson is over for today,</q> he said.</p>
+    </>
+  )
+  .addNextChapter(
+    <>
+      <p>Prince Wang's programmer was coding software. His fingers danced upon the keyboard. The program compiled without and error message, and the program ran like a gentle wind.</p>
+      <p><q>Excellent!</q> the Prince exclaimed. <q>Your technique is faultless!</q></p>
+      <p><q>Technique?</q> said the programmer, turning from his terminal, <q>What I follow is Tao -- beyond all techniques! When I first began to program, I would see before me the whole problem in one mass. After three years, I no longer saw this mass. Instead, I used subroutines. But now I see nothing. My whole being exists in a formless void. My senses are idle. My spirit, free to work without a plan, follows its own instinct. In short, my program writes itself. True, sometimes there are difficult problems. I see them coming, I slow down, I watch silently. Then I change a single line of code and the difficulties vanish like puffs of idle smoke. I then compile the program. I sit still and let the joy of the work fill my being. I close my eyes for a moment and then log off.</q></p>
+      <p>Prince Wang said, <q>If only all of my programmers were as wise!</q></p>
+    </>
+  );
 
 export const book5 =
   Book.new(
