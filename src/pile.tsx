331ce3dfdfbee7ae57557d926e8b78a15ac616bf
Charles Bayley <cbayley95@gmail.com>
2023-07-18 19:25:43 -0400

    Style

--- src/pile.tsx
+++ src/pile.tsx
@@ -86,7 +86,7 @@ export const book2 =
       <p>A novice asked the Master: <q>Here is a programmer that never designs, documents or tests his programs. Yet all who know him consider him one of the best programmers in the world. Why is this?</q></p>
       <p>The Master replied: <q>That programmer has mastered the Tao. He has gone beyond the need for design; he does not become angry when the system crashes, but accepts the universe without concern. He has gone beyond the need for documentation; he no longer cares if anyone else sees his code. He has gone beyond the need for testing; each of his programs are perfect within themselves, serene and elegant, their purpose self-evident. Truly, he has entered the mystery of Tao.</q></p>
     </>
-  )
+  );
 
 export const book3 =
   Book.new(
