53a6f9bd0b29fc21625dd1817ea299c62a04ed95
Charles Bayley <cbayley95@gmail.com>
2023-07-21 02:13:39 -0400

    Set a metadescription

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { GetStaticProps, Metadata } from 'next';
+import { Metadata } from 'next';
 
 import pile from '@/pile';
 import Pile from '@components/pile/pile';
 
 export const metadata: Metadata = {
   title: 'The Tao of Programming',
+  description: 'The Tao of Programming: Learn about the programmers of old.'
 };
 
 export default function Page({}) {
